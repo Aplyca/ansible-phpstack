e07b869ffd13d0e205c439458314f607c2e71a80
Mauricio Sánchez <msanchez@aplyca.com>
2016-10-23 08:41:02 -0500

    Improving variables and tests

--- build/vagrant.sh
+++ build/vagrant.sh
@@ -7,17 +7,18 @@ vagrant destroy -f
 vagrant up
 
 echo "Packaging box"
-rm -rf ezdev.web.box
-vagrant package --output ezdev.web.box
+rm -rf ezapp.web.box
+vagrant package --output ezapp.web.box
 
 echo "Destroying machine"
 vagrant destroy -f
 
 echo "Replacing box"
 vagrant box remove Aplyca/ezapp.web
-vagrant box add ezdev.web.box --name Aplyca/ezdev.web
+vagrant box add ezapp.web.box --name Aplyca/ezapp.web
 
 echo "Testing box"
 cd tests
 vagrant up
-vagrant ssh
+vagrant ssh -- "ps aux | grep 'apache|mysql'"
+vagrant ssh -- "php -v"
