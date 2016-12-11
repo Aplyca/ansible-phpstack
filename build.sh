25156748dd786e075285366ad4de92b7c8184709
Mauricio Sanchez <msanchez@aplyca.com>
2016-12-11 17:47:48 -0500

    Fixing vagrant tests

--- build.sh
+++ build.sh
@@ -14,11 +14,12 @@ echo "Destroying machine"
 vagrant destroy -f
 
 echo "Replacing box"
+cd tests
+vagrant destroy -f
 vagrant box remove Test/LAMPDeven
 vagrant box add lampdeven.box --name Test/LAMPDeven
 
 echo "Testing box"
-cd tests
 vagrant up --no-provision
 vagrant ssh -- "ps aux | grep -E 'apache|mysql'"
 vagrant ssh -- "php -v"
